f86f423d3a2e597da601422fe5b5bbafdc485e93
jonathasluis <jonathas_sousasgs@hotmail.com>
2024-07-02 20:57:25 -0300

    rename

---
+++ alura-space/galeria/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    dados = {
+        1 : {"nome" : "Nebulosa de Carina",
+            "legenda" : "webbtelecope / NASA / JAMES Webb"},
+        2 : {"nome" : "Galaxia NGC 1079",
+            "legenda" : "nasa.org / NASA / Hubble"}
+    }
+
+    return render(request, 'galeria/index.html', {"cards" : dados})
+
+def imagem(request):
+    return render(request, 'galeria/imagem.html')
